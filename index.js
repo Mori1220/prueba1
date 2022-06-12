a78a736c75e55ea9a5f434475b6c598b7d01be3a
Karla Moriana Rodriguez Yllescas <moriAeseces@gmail.com>
2022-06-12 06:18:51 -0500

    added unshift method definition, use case and example

---
+++ index.js
@@ -0,0 +1,9 @@
+//Unshift: El método unshift() agrega uno o más elementos al inicio del array, y devuelve el nuevo número de datos guardados del array
+
+//Caso de uso: tenemos un array con x cantidad de números guardados ahí y queremos implementarle x cantidad mas de números que ahora serán agregados al inicio del primer array junto con la nueva longitud del array.
+
+const num = [552, 342, 663];
+
+console.log(num.unshift(400, 556, 100));
+
+console.log(num);
