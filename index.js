cc75b72343544b22a5c939eaf769bf10da71d4d0
Karla Moriana Rodriguez Yllescas <moriAeseces@gmail.com>
2022-06-12 06:33:01 -0500

    Added promises method definition, use case and example

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-//Hoisting: El hoisting es un nombre utilizado para referirse a cómo funcionan los contextos de ejecución en JavaScript para la creación y ejecución de funciones y variables.
+//Promesas: Las promesas se crean usando un constructor llamado Promise y pasándole una función que recibe dos parámetros, resolve y reject , que nos permiten indicarle a esta que se resolvió o se rechazó.
 
-//Caso de uso: En este caso podemos llamar a una función y definirla más abajo, porque automáticamente JS la “subirá”. Así, este código funciona correctamente.
+//Caso de uso: En este caso se creó una promesa que se completará en 2 segundos. Si el número aleatorio que hemos generado es mayor a 4 se resolverá, en caso contrario se rechaza y se muestra un error.
 
+const promise = new Promise((resolve, reject) => {
+  const number = Math.floor(Math.random() * 12);
+  setTimeout(
+                 () => number > 4
+                                 ? resolve(number)
+                                 : reject(new Error('Menor a 4')),
+                 2000
+  );
+});
 
-function add() {
-  var Num;
-  Num = 2;
-  console.log(Num + Num);
-}
-add();
+promise
+  .then(number => console.log(number))
+  .catch(error => console.error(error));
